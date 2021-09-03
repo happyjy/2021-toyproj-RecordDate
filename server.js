874daf8fca77d77e2aee24ef6e55f308820c360a
happyjy <okwoyjy@gmail.com>
2021-09-03 22:48:32 +0900

    heroku mysql 설정

--- server.js
+++ server.js
@@ -42,25 +42,7 @@ if (env !== "dev") {
     multipleStatements: true,
   });
 
-  function handleDisconnect() {
-    connection.connect(function (err) {
-      if (err) {
-        console.log("error when connecting to connection:", err);
-        setTimeout(handleDisconnect, 2000);
-      }
-    });
-
-    connection.on("error", function (err) {
-      console.log("connection error", err);
-      if (err.code === "PROTOCOL_CONNECTION_LOST") {
-        return handleDisconnect();
-      } else {
-        throw err;
-      }
-    });
-  }
-
-  handleDisconnect();
+  connection.connect();
   // connection = dbconfig;
   // connection.connect();
 
