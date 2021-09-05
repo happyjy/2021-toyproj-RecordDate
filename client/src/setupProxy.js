b562ff1e6287fc04492d3e2498adbc15d533b2e4
happyjy <okwoyjy@gmail.com>
2021-09-05 22:32:45 +0900

    heroku mysql 설정 - requestUrl 수정 적용

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,6 +1,11 @@
-// const createProxyMiddleware = require('http-proxy-middleware');
+const createProxyMiddleware = require('http-proxy-middleware');
 
-// module.exports = function (app) {
-//   console.log('### createProxyMiddleware ###', app);
-//   app.use(createProxyMiddleware('/api', { target: 'http://localhost:5000' }));
-// };
+module.exports = function (app) {
+  console.log('### createProxyMiddleware ###', app);
+  // app.use(createProxyMiddleware('/api', { target: 'http://localhost:5000' }));
+  app.use(
+    createProxyMiddleware('/api', {
+      target: 'https://ourdatinghistory.herokuapp.com/api/test',
+    }),
+  );
+};
